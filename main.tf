cb274c7b492464a55a8011349b659e73ca8e9064
Aviz <avizway@gmail.com>
2026-07-23 20:38:52 +0530

    bucket create private

--- main.tf
+++ main.tf
@@ -27,25 +27,3 @@ resource "aws_instance" "mumbai-webserver" {
 resource "aws_s3_bucket" "public_bucket" {
   bucket = "aviz-hcp-test-23072026"
 }
-
-resource "aws_s3_bucket_public_access_block" "this" {
-  bucket                  = aws_s3_bucket.public_bucket.id
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
-}
-
-resource "aws_s3_bucket_policy" "this" {
-  bucket     = aws_s3_bucket.public_bucket.id
-  depends_on = [aws_s3_bucket_public_access_block.this]
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Effect    = "Allow"
-      Principal = "*"
-      Action    = "s3:GetObject"
-      Resource  = "${aws_s3_bucket.public_bucket.arn}/*"
-    }]
-  })
-}
